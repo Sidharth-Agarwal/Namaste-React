daf1893416a30f155f42c104d4c27a8e8f95f5a5
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-02-28 00:26:40 +0530

    Starting with React

--- 2. Igniting Your App/app.js
+++ 2. Igniting Your App/app.js
@@ -1,42 +1,9 @@
-// Our main aim is to create a structure like this
-
-/*
-<div id="parent">
-    <div id="child2">
-        <h1>I'm the main H1 tag</h1>
-        <h2>I'm the main H2 tag</h2>
-    </div>
-    <div id="child1">
-        <h1>I'm the main H1 tag</h1>
-        <h2>I'm the main H2 tag</h2>
-    </div>
-</div>
-*/
-
-// When we have to create sibling in a tag
-// We have to make an array in the third argument of the createElement function
-// Then we have to define all the children of the parent individually in the array
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id:"child1"},[
-        React.createElement("h1", {}, "I'm the main H1 tag"),
-        React.createElement("h2", {}, "I'm the main H2 tag")
-    ]),React.createElement("div", {id:"child2"},[
-        React.createElement("h1", {}, "I'm the main H1 tag"),
-        React.createElement("h2", {}, "I'm the main H2 tag")
-    ])
-]);
+// React.createElement => Creates an object => Render on DOM => HTML Element
 
-// The code all throw an error in the console for the missing "key"
+const heading = React.createElement("h1", {id:"heading"}, "Sidharth is here...!");
 
-// This is a very tedious and difficult to-read way of writing react in JS
-// For that "JSX" was introduced
-// The "JSX" thing will be discuss in further lectures
-
-// const parent = React.createElement("", {}, "")
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
