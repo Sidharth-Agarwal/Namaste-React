60f6e3bc2170388242f0489e5b01207ce166bdd9
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-02-28 17:11:18 +0530

    Functional Components

--- 2. Igniting Your App/app.js
+++ 2. Igniting Your App/app.js
@@ -15,9 +15,30 @@ const heading = (
 // (Always name a component in capital letters)
 // A component is nothing but a piece of code which return a piece of JSX
 // So, basically a function which returns a react element is a functional component
-const HeadingComponent = () => {
-    return <h1> Namaste React Functional Component </h1>;
-}
+const HeadingComponent = () => (
+    <div id="container">
+        <h1 className="heading"> Namaste React Functional Component </h1>
+    </div>
+);
+
+// This can also be written as - 
+// 1.
+// const HeadingComponent = () => <h1> Namaste React Functional Component </h1>;
+// 2.
+// const HeadingComponent = () => {
+//     return <h1> Namaste React Functional Component </h1>;
+// }
+
+// These two functions are same -
+// 1.
+// const fn = () => true;
+// 2.
+// const fn2 = () => {
+//     return true;
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+// Rendering a component on the browser
+console.log(HeadingComponent);
+root.render(<HeadingComponent/>)
