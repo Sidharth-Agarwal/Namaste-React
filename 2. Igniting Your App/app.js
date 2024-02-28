b84a29548712aa96a3a8f1572ebd4865dd848be8
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-02-28 15:34:23 +0530

    Adding JSX transpilation flow

--- 2. Igniting Your App/app.js
+++ 2. Igniting Your App/app.js
@@ -6,6 +6,18 @@ const heading = React.createElement("h1", {id:"heading"}, "Sidharth is here...!"
 
 // JSX - HTML-like or XML-like syntax in JavaScript
 const jsxHeading = <h1>Hello world from JSX...!</h1>
+// The JS engine will not understand the above line because JS engine only understands ECMA Script.
+// So what happens is - Even before this code goes to the JS Engine is it transpiled
+// Then, JS Engine receives a code which the browsers can understands
+// This transpilation of code is done with the help of Parcel
+// However, parcel doesn't do the process by itself. It is done with the help of a package called "Babel"
+
+// Another piece of information - 
+// The flow of the rendering a JSX file onto the browser goes in the following steps :
+// The JSX is transpiled into a React.createElement
+// This React.createElement create a ReactElement which is JS Object
+// This is then finally rendered into a HTML element
+// JSX -> React.createElement -> ReactElement - JS Object -> HTML Element(render)
 
 console.log(heading);
 console.log(jsxHeading);
