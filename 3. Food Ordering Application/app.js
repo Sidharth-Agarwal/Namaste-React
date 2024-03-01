6a6c13c911e009e39e363f528fb2f7792ab3b370
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-03-01 18:37:58 +0530

    Starting with the applications

---
+++ 3. Food Ordering Application/app.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+High Level Layout -:
+
+-> Header
+1. Logo
+2. Nav Items
+
+-> Body
+1. Search
+2. Restaurant Container
+    a. Restaurant Card
+
+-> Footer
+1. Copyright
+2. Links
+3. Address
+4. Contact
+*/
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
