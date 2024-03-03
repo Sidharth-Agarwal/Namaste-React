a321cf4465e008062422a015410280b259feb754
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-03-03 16:23:32 +0530

    Added a number of cards

--- 3. Food Ordering Application/app.js
+++ 3. Food Ordering Application/app.js
@@ -65,6 +65,19 @@ const Body = () => {
             <div className="search">Search</div>
             <div className="res-container">
                 <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
             </div>
         </div>
     )
