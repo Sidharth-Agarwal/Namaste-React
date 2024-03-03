9906852468ac13005a8e9086357c96539f03072b
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-03-04 03:47:08 +0530

    Changed the application structure and file system

---
+++ 3. Food Ordering Application/src/components/Body.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockdata";
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {resList.map((restaurant) => (
+                    <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Body;
---
+++ 3. Food Ordering Application/src/components/RestaurantCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+    const {resData} = props
+
+    const {
+        cloudinaryImageId,
+        name,
+        avgRating,
+        cuisines,
+        costForTwo,
+        deliveryTime
+    } = resData?.data;
+
+    return (
+        <div className="res-card">
+            <img className="res-logo" src={CDN_URL + resData.data.cloudinaryImageId}/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(', ')}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>₹{costForTwo / 100} for two</h4>
+            <h4>{deliveryTime} minutes</h4>
+        </div>
+    )
+}
+
+export default RestaurantCard;
