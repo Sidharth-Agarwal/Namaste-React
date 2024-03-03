9d8f9410385bb01c22171a36a3afbec5b99ef2e4
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-03-03 19:40:53 +0530

    Adding props

--- 3. Food Ordering Application/app.js
+++ 3. Food Ordering Application/app.js
@@ -47,14 +47,47 @@ const styleCard = {
     backgroundColor:"#f0f0f0"
 }
 
-const RestaurantCard = () => {
+// This is the way of making a static component
+// const RestaurantCard = () => {
+//     return (
+//         <div className="res-card" style={styleCard}>
+//             <img className="res-logo" src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg"/>
+//             <h3>Meghana Foods</h3>
+//             <h4>Biryani, North Indian, Asian</h4>
+//             <h4>4.3 Stars</h4>
+//             <h4>38 minutes</h4>
+//         </div>
+//     )
+// }
+
+// Creating dynamic cards using props
+// Here was are passing props into a react component
+// These props act an object in the component
+
+// This is called de-structing on the fly
+// This method can also be used while using props
+// const RestaurantCard = ({resName, cuisine, rating, delivery}) => {
+//     return (
+//         <div className="res-card" style={styleCard}>
+//             <img className="res-logo" src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg"/>
+//             <h3>{resName}</h3>
+//             <h4>{cuisine}</h4>
+//             <h4>{rating}</h4>
+//             <h4>{delivery}</h4>
+//         </div>
+//     )
+// }
+
+// Another method
+const RestaurantCard = (props) => {
+    console.log(props)
     return (
         <div className="res-card" style={styleCard}>
             <img className="res-logo" src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg"/>
-            <h3>Meghana Foods</h3>
-            <h4>Biryani, North Indian, Asian</h4>
-            <h4>4.3 Stars</h4>
-            <h4>38 minutes</h4>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.rating}</h4>
+            <h4>{props.delivery}</h4>
         </div>
     )
 }
@@ -64,20 +97,18 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
+                <RestaurantCard 
+                    resName="Meghana Food" 
+                    cuisine="Biryani, North Indian, Asian" 
+                    rating="4.3" 
+                    delivery="38 minutes"
+                />
+                <RestaurantCard 
+                    resName="KFC" 
+                    cuisine="Burger, Fast Food" 
+                    rating="4.1" 
+                    delivery="21 minutes"
+                />
             </div>
         </div>
     )
