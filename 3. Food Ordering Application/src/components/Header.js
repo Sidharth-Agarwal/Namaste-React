376c2aeb825cce04169a250134736e631bd27f5f
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-03-10 17:00:40 +0530

    Reason why useState is required in React

--- 3. Food Ordering Application/src/components/Header.js
+++ 3. Food Ordering Application/src/components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+    // const btnName = "Login";
+    // This is the reason why local state variables are used to update data dynamically
+    const [btnNameReact, setBtnNameReact] = useState("Login")
+    // When the button is click the whole header component is rendered
+    // But since the only the login button is updated only that value will be affects
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -14,6 +21,13 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    {/* We can update/re-render the page using JavaScript variables
+                    <button className="login-btn" onClick={() => {btnName="Logout"}}>
+                        {btnName}
+                    </button> */}
+                    <button className="login-btn" onClick={() => {setBtnNameReact("Logout")}}>
+                        {btnNameReact}
+                    </button>
                 </ul>
             </div>
         </div>
