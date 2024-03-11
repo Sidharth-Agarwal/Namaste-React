276ba5701e65c4c8c9d8c4176928f86f499633b8
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-03-12 03:54:34 +0530

    Jargon about hooks

--- 3. Food Ordering Application/src/components/Header.js
+++ 3. Food Ordering Application/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { LOGO_URL } from "../utils/constants";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// Never create state variable outside the component
+// const [btnNameReact, setBtnNameReact] = useState("Login")
+// This will throw an error
 
 const Header = () => {
     // const btnName = "Login";
@@ -9,6 +13,20 @@ const Header = () => {
     const [btnNameReact, setBtnNameReact] = useState("Login")
     // When the button is click the whole header component is rendered
     // But since the only the login button is updated only that value will be affects
+
+    // Always keep the state variables on the top of the component
+    // Don't create state variable in conditionals or loops - This create inconsistencies
+    
+    // Important stuff about useEffect -:
+    // If no dependency array is given
+    // Evertime the header component is rendered - useEffect will be called
+    // If dependency array is given
+    // useEffect is only called on the initial render - Just Once
+    // If dependency array is [btnNameReact]
+    // useEffect will be called everytime btnNameReact will be updated
+    useEffect(() => {
+        console.log("useEffect Called!")
+    }, []);
 
     return (
         <div className="header">
