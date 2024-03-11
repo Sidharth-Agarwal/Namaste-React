0b7f317651fd9b1c992c412f982fa3ac5b67e157
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-03-11 20:42:22 +0530

    Added Comment

--- 3. Food Ordering Application/src/components/Body.js
+++ 3. Food Ordering Application/src/components/Body.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockdata";
+// import resList from "../utils/mockdata";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
