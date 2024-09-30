a2fe9acee3b1c434150e47f0233ef9efd962c812
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2024-09-30 14:53:38 +0530

    Creating more complex structure

--- 1. Basics/6MoreComplexStructures.js
+++ 1. Basics/6MoreComplexStructures.js
@@ -2,17 +2,33 @@
 
 /* <div id="parent">
     <div id="child1">
-        <h1 class="sibling">I'm h1 Tag</h1>
-        <h2 class="sibling">I'm h2 Tag</h2>
+        <h1 class="sibling">I'm sibling 1 from child1</h1>
+        <h2 class="sibling">I'm sibling 2 from child1</h2>
     </div>
     <div id="child2">
-        <h1 class="sibling">I'm h1 Tag</h1>
-        <h2 class="sibling">I'm h2 Tag</h2>
+        <h1 class="sibling">I'm sibling 1 from child2</h1>
+        <h2 class="sibling">I'm sibling 2 from child2</h2>
     </div>
 </div> */
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-const parent = React.createElement("h1", {} ,
-    
+const parent = React.createElement("div", {id:"parent"} ,
+    [
+        "Hi from parent tag",
+        React.createElement("div", {id:"child1"}, 
+            [
+                React.createElement("h1", {class:"sibling"}, "I'm sibling 1 from child1"),
+                React.createElement("h2", {class:"sibling"}, "I'm sibling 2 from child1")
+            ]
+        ),
+        React.createElement("div", {id:"child2"}, 
+            [
+                React.createElement("h1", {class:"sibling"}, "I'm sibling 1 from child2"),
+                React.createElement("h2", {class:"sibling"}, "I'm sibling 2 from child2")                
+            ]
+        )
+    ]
 )
+
+root.render(parent)
