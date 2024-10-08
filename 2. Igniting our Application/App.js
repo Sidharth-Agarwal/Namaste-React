029c460829821a0853aa6999cf287096c25afb6e
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-10-08 15:33:20 +0530

    Creating first parcel builts with CDN Links

---
+++ 2. Igniting our Application/App.js
@@ -0,0 +1,34 @@
+// Creating this structure using React.createElement
+
+/* <div id="parent">
+    <div id="child1">
+        <h1 class="sibling">I'm sibling 1 from child1</h1>
+        <h2 class="sibling">I'm sibling 2 from child1</h2>
+    </div>
+    <div id="child2">
+        <h1 class="sibling">I'm sibling 1 from child2</h1>
+        <h2 class="sibling">I'm sibling 2 from child2</h2>
+    </div>
+</div> */
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const parent = React.createElement("div", {id:"parent"},
+    [
+        "Hi from parent tag",
+        React.createElement("div", {id:"child1"}, 
+            [
+                React.createElement("h1", {class:"sibling"}, "I'm sibling 1 from child1"),
+                React.createElement("h2", {class:"sibling"}, "I'm sibling 2 from child1")
+            ]
+        ),
+        React.createElement("div", {id:"child2"},
+            [
+                React.createElement("h1", {class:"sibling"}, "I'm sibling 1 from child2"),
+                React.createElement("h2", {class:"sibling"}, "I'm sibling 2 from child2")
+            ]
+        )
+    ]
+)
+
+root.render(parent)
