e7a6324b4d241731cafc834c6f6f8c270642d98a
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2024-10-10 10:06:58 +0530

    Working with JSX

--- 3. Laying the Foundation/app.js
+++ 3. Laying the Foundation/app.js
@@ -1,2 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+
+// Creating React element using React.createElement
+// ---------------------------------------------------------------------------------
+// const heading = React.createElement("h1", {id:"heading"}, "This is Namaste React")
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+
+// JSX is HTML-like or XML-like Syntax
+// Creating React Element using JSX
+// JSX(transpilation - By Parcel using Babel) => React Element - JS Object => HTML Element(on render)
+// --------------------------------------------------------------------------------------------------
+const jsxHeading = <h1 id="heading"> Hello from JSX </h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(jsxHeading)
