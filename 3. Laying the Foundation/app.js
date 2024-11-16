87db5adeeabc5283e4dc46e42206e54b8763d41f
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-11-17 00:57:17 +0530

    Minor update

--- 3. Laying the Foundation/app.js
+++ 3. Laying the Foundation/app.js
@@ -1,2 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+
+// React.createElement => JavaScript Object => HTMLElement(render)
+
+// Creating a React Element
+// This is creating react element using Core React
+// --------------------------------------------------------------------------------------
+// const heading = React.createElement("h1", {id:"heading"}, "Hello from React Element!")
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+
+// JSX - HTML-like or XML-like syntax
+// This is creating react element using JSX
+// ---------------------------------------------------------------
+const jsxHeading = <h1 id="heading"> Hello World from JSX</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(jsxHeading)
