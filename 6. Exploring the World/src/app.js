e271291f9803aae871207efdf681991d909e14ff
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2025-03-02 00:39:57 +0530

    lecture 6

---
+++ 6. Exploring the World/src/app.js
@@ -0,0 +1,32 @@
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - Restaurant Container
+ *   - Restaurant Cards
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout/>)
---
+++ 6. Exploring the World/src/components/Body.js
@@ -0,0 +1,44 @@
+import RestaurantCard from "./RestaurantCard";
+import restaurantList from "../utils/mockData";
+import {useState, useEffect} from "react"
+
+const Body = () => {
+  // Local (inside the component) State variable - Super powerful variable
+  const arr = useState(restaurantList)
+  // const [listOfRestaurant ,setListOfRestaurant] = arr;
+
+  const listOfRestaurant = arr[0];
+  const setListOfRestaurant = arr[1]
+
+  useEffect(()=>{
+    console.log("use effect called!")
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const jsonData = await data.json()
+  };
+
+  console.log(fetchData)
+
+  return(
+      <div className="body">
+        <div>
+          <button className="filter-btn" onClick={()=>{
+            const filteredList = listOfRestaurant.filter(
+              (res) => (res.data.avgRating) > 4
+            )
+            setListOfRestaurant(filteredList)
+          }}>Top Rated Restaurants</button>
+        </div>
+        {/* <div className="search">Search</div> */}
+        <div className="res-container">
+          {listOfRestaurant.map((restaurant) => {
+            return <RestaurantCard key={restaurant.data.id} {...restaurant?.data} />
+          })}
+        </div>
+      </div>
+  )
+}
+
+export default Body;
